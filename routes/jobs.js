9337654e952a14b8f0f6680f0a327d71f04029d3
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-04-06 21:38:40 +0530

    updating jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,11 +6,13 @@ const {
   getJobs,
   newJob,
   getJobsInRadius,
+  updateJob,
 } = require("../controllers/jobsController");
 
 router.route("/jobs").get(getJobs);
 router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
 
 router.route("/job/new").post(newJob);
+router.route("/job/:id").put(updateJob);
 
 module.exports = router;
