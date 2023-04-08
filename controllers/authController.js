b7d2c70b3789cced782e547f3c70a35a343e509c
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-04-08 23:42:22 +0530

    user login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorHandler");
 
 // Register a new user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -18,6 +19,38 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "User registered successfully",
+    token,
+  });
+});
+
+// Login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email or password is entered by the user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password"), 400);
+  }
+
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+
+  // Check if password is correct
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+
+  // Create JSON Web Token
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    success: true,
     token,
   });
 });
--- models/users.js
+++ models/users.js
@@ -49,4 +49,9 @@ userSchema.methods.getJwtToken = function () {
   });
 };
 
+// Compare user password with database password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
